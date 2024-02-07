7d1cceb8c6d7c4e64d0ef1d452dbab07dd9606b5
FCICoder <mahmoud.aadel1998@gmail.com>
2024-02-08 00:49:35 +0200

    updates11

---
+++ app/[lng]/about-us/page.jsx
@@ -0,0 +1,63 @@
+import { imageOptimizer } from "next/dist/server/image-optimizer";
+import Image from "next/image";
+import React from "react";
+import Astyles from './about.module.css'
+import olives from '@/public/assets/assorted-olives.jpg'
+import { useTranslation } from "@/app/i18n";
+
+export default async function Page({params:{lng}}) {
+  const { t } = await useTranslation(lng, "about-us");
+
+  return <>
+
+<div id="container-aboutus">
+  <div className="row m-auto my-5">
+    <div className="col-md-6 " >
+      <h3  className={`${lng === 'en' ? Astyles.head1 : Astyles.head1Ar} `}>{t('part1')}</h3>
+      <p className={`${Astyles.para} pt-3`}>{t('part1.1')}</p>
+      <p className={`${Astyles.para}`}>{t('part2')}</p>
+      <p className={`${Astyles.para}`}>{t('part3')}</p>
+    </div>
+    <div className=" col-md-6 p-1 text-center">
+    <Image
+      src={olives}
+      width={500}
+      height={400}
+      alt="assorted-olives"
+      className="img-fluid rounded center"
+    />
+    </div>
+
+  </div>
+  <div className="row my-5 mx-0 p-0">
+    <div className="col-md-6 my-2">
+      <h3  className={`${lng === 'en' ? Astyles.head1 : Astyles.head1Ar}`}>{t('part4')}</h3>
+      <p className={`${Astyles.para} pt-3`}>{t('part5')}</p>
+
+
+    </div>
+    <div className="col-md-6 mb-2">
+    <h3  className={`${lng === 'en' ? Astyles.head1 :Astyles.head1Ar}`}>{t('part6')}</h3>
+      <p className={`${Astyles.para} pt-3`}>{t('part7')}</p>
+    </div>
+    <div className="col-md-6 mb-2">
+    <h3  className={`${lng === 'en' ? Astyles.head1 : Astyles.head1Ar}`}>{t('part8')}</h3>
+      <p className={`${Astyles.para} pt-3`}>{t('part9')}</p>
+    </div>
+    <div className="col-md-6 mb-5">
+    <h3  className={`${lng === 'en' ? Astyles.head1 : Astyles.head1Ar}`}>{t('part10')}</h3>
+      <p className={`${Astyles.para} pt-3`}>{t('part11')}</p>
+    </div>
+    <div className="col-md-6 mb-5">
+    <h3 className={`${lng === 'en' ? Astyles.head1 : Astyles.head1Ar}`}>{t('part12')}</h3>
+      <p className={`${Astyles.para} pt-3`}>{t('part13')}</p>
+    </div>
+    <div className="col-md-6 mb-5">
+    <h3 className={`${lng === 'en' ? Astyles.head1 : Astyles.head1Ar}`}>{t('part14')}</h3>
+      <p className={`${Astyles.para} pt-3`}>{t('part15')}</p>
+    </div>
+  </div>
+  </div>
+  
+  </>;
+}
---
+++ app/[lng]/products/page.jsx
@@ -0,0 +1,288 @@
+"use client"
+import React, { useState } from 'react';
+import products from './products.json';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Page({ params: { lng } }) {
+  const [hoveredImages, setHoveredImages] = useState(new Array(products.products.length).fill(false));
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handleMouseEnter = (index, imgSrc) => {
+    const newHoveredImages = [...hoveredImages];
+    newHoveredImages[index] = imgSrc;
+    setHoveredImages(newHoveredImages);
+  };
+
+  const handleMouseLeave = (index) => {
+    const newHoveredImages = [...hoveredImages];
+    newHoveredImages[index] = false;
+    setHoveredImages(newHoveredImages);
+  };
+
+  const handleImageClick = (prd) => {
+    setSelectedProduct(prd);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedProduct(null);
+  };
+
+  const handleNextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % selectedProduct.en.images.length);
+  };
+
+  const handlePrevImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + selectedProduct.en.images.length) % selectedProduct.en.images.length);
+  };
+
+  const [activeIcon, setActiveIcon] = useState('grid');
+
+  const handleIconClick = (icon) => {
+    setActiveIcon(icon);
+  };
+  return (<>
+    <div>
+      <p id="prd_p">{
+        (lng == 'en') ? "Ready to enhance your product offerings with premium pickles and preserves? Explore our wide range of olives, cucumbers, peppers, carrots, and turnips to find the perfect options for your business. Whether you are a pickle factory or a customer in need of large quantities in drums, Makdouz is here to fulfill your requirements."
+          :
+          "هل أنت على استعداد لتعزيز عروض منتجاتك من خلال المخللات والمعلبات المتميزة؟ استكشف مجموعتنا الواسعة من الزيتون والخيار والفلفل والجزر واللفت للعثور على الخيارات المثالية لعملك. سواء كنت مصنع مخللات أو عميلاً يحتاج إلى كميات كبيرة من البراميل، مكدوز هنا لتلبية متطلباتك."
+      }
+      </p>
+      <div id='grid_system'>
+        <i
+          className={`fas fa-th pointer ${activeIcon === 'grid' ? 'active' : ''}`}
+          aria-hidden='true'
+          role='button'
+          onClick={() => handleIconClick('grid')}
+        ></i>
+
+        <i
+          className={`fas fa-list ${activeIcon === 'list' ? 'active' : ''}`}
+          aria-hidden='true'
+          role='button'
+          onClick={() => handleIconClick('list')}
+        ></i>
+      </div>
+      {
+        activeIcon === 'list' ?
+          <div className='mt-4 ps-3'>
+            <div className='row g-5 d-flex flex-column '>
+              {products.products?.map((prd, index) => <div className='row ' key={index} >
+                  <div className="col-md-4 " >
+                    <div className=' '>
+                      {(lng === 'en') ? (
+                        <div className=' text-center   ' id="connnte" >
+                          <Link 
+                          href={{
+                            pathname: `/${lng}/productDetails`,
+                            query: { id: `${prd.id}` },
+                          }}
+                          id='prd_Link1' className='  '>
+                            <div id='content-prds' className='' >
+                              <Image
+                                id='prd_image'
+                                className='pb-4 '
+                                src={hoveredImages[index] || prd.en.img1}
+                                width={500}
+                                height={200}
+                                style={{ width: 'auto', height: "auto" }}
+                                onMouseEnter={() => handleMouseEnter(index, prd.en.img2)}
+                                onMouseLeave={() => handleMouseLeave(index)}
+                                alt='makdouz products'
+                              />
+
+                            </div>
+                            </Link>
+                            <div id='icon' className='m-0'>
+                              <span className=' d-flex justify-content-center text-uppercase' onClick={() => handleImageClick(prd)}>
+                                <i className="far fa-eye mx-3" aria-hidden="true" role="img"></i>
+                                <p className=' m-0  m-0 ' style={{ fontSize: "14px" }}>
+                                  Quick view
+                                </p>
+                              </span>
+                            </div>
+                          
+
+                        </div>
+                      ) : (
+                        <div className=' text-center  ' id="connnte" >
+                          <Link 
+                           href={{
+                            pathname: `/${lng}/productDetails`,
+                            query: { id: `${prd.id}` },
+                          }}
+                          id='prd_Link1' className=' '>
+                            <div id='content-prds' >
+                              <Image
+                                id='prd_image'
+                                className='pb-4'
+                                src={hoveredImages[index] || prd.ar.img1}
+                                width={500}
+                                height={200}
+                                style={{ width: 'auto', height: "auto" }}
+                                onMouseEnter={() => handleMouseEnter(index, prd.ar.img2)}
+                                onMouseLeave={() => handleMouseLeave(index)}
+                                alt='منتجات مكدوز'
+                              />
+                            </div>
+                          </Link>
+
+                            <div id='icon' className='m-0'>
+                              <span className=' d-flex justify-content-center text-uppercase' onClick={() => handleImageClick(prd)}>
+                                <i className="far fa-eye mx-3" aria-hidden="true" role="img"></i>
+                                <p className=' m-0  m-0 ' style={{ fontSize: "14px" }}>
+                                  نظره سريعه
+                                </p>
+                              </span>
+                            </div>
+
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                  <div className=' col-md-8    d-flex justify-content-center align-items-center flex-column '>
+                    <span id='prd_Link' className='p-1' style={{ fontSize: "24px" }}>
+                      {
+                        (lng == 'en') ?
+                          prd.en.name
+                          :
+                          prd.ar.name
+                      }
+                    </span>
+                    <p className='mt-4' style={{fontSize:"14px"}}>
+                      {
+                        (lng == 'en') ?
+                          prd.en.description
+                          :
+                          prd.ar.description
+                      }
+                    </p>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+          :
+          <div className='mt-4'>
+            <div className='row g-5 '>
+              {products.products?.map((prd, index) => (
+                <div className="col-md-4 " key={index}>
+                  <div>
+                    {(lng === 'en') ? (
+                      <div className=' text-center  ' id="connnte" >
+                        <Link 
+                         href={{
+                          pathname: `/${lng}/productDetails`,
+                          query: { id: `${prd.id}` },
+                        }}
+                        id='prd_Link1' className=' '>
+                          <div id='content-prds' >
+                            <Image
+                              id='prd_image'
+                              className='pb-4'
+                              src={hoveredImages[index] || prd.en.img1}
+                              width={500}
+                              height={200}
+                              style={{ width: 'auto', height: "auto" }}
+                              onMouseEnter={() => handleMouseEnter(index, prd.en.img2)}
+                              onMouseLeave={() => handleMouseLeave(index)}
+                              alt='makdouz products'
+                            />
+                          </div>
+                        </Link>
+
+                          <div id='icon' className='m-0'>
+                            <span className=' d-flex justify-content-center text-uppercase' onClick={() => handleImageClick(prd)}>
+                              <i className="far fa-eye mx-3" aria-hidden="true" role="img"></i>
+                              <p className=' m-0  m-0 ' style={{ fontSize: "14px" }}>
+                                Quick view
+                              </p>
+                            </span>
+                          </div>
+                        <div>
+                          <span id='prd_Link'>
+                            {prd.en.name}
+                          </span>
+                        </div>
+                      </div>
+                    ) : (
+                      <div className=' text-center  ' id="connnte" >
+                        <Link  
+                        href={{
+                            pathname: `/${lng}/productDetails`,
+                            query: { id: `${prd.id}` },
+                          }}
+                           id='prd_Link1' className=' '>
+                          <div id='content-prds' >
+                            <Image
+                              id='prd_image'
+                              className='pb-4'
+                              src={hoveredImages[index] || prd.ar.img1}
+                              width={500}
+                              height={200}
+                              style={{ width: 'auto', height: "auto" }}
+                              onMouseEnter={() => handleMouseEnter(index, prd.ar.img2)}
+                              onMouseLeave={() => handleMouseLeave(index)}
+                              alt='منتجات مكدوز'
+                            />
+                          </div>
+                        </Link>
+
+                          <div id='icon' className='m-0'>
+                            <span className=' d-flex justify-content-center text-uppercase' onClick={() => handleImageClick(prd)}>
+                              <i className="far fa-eye mx-3" aria-hidden="true" role="img"></i>
+                              <p className=' m-0  m-0 ' style={{ fontSize: "14px" }}>
+                                نظره سريعه
+                              </p>
+                            </span>
+                          </div>
+                        <div>
+                          <span id='prd_Link'>
+                            {prd.ar.name}
+                          </span>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+      }
+
+      {selectedProduct && (
+        <div className="modal" style={{ display: 'block', backgroundColor: 'rgba(0, 0, 0, 0.5)', position: 'fixed', top: 0, left: 0, right: 0, bottom: 0 }}>
+          <div className="modal-dialog" style={{ maxWidth: '80%', margin: 'auto', marginTop: '5vh' }}>
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="btn-close" aria-label="Close" onClick={handleCloseModal}></button>
+              </div>
+              <div className="modal-body">
+                <div className="text-center  d-flex justify-content-center align-items-center h-100">
+                  <div id='layout_image' className='bg-black my-auto'>
+                  
+                  <i role='button' className="  fas fa-chevron-left" onClick={handlePrevImage}></i>
+                    <Image
+                      id='image_out'
+                      src={selectedProduct.en.images[currentImageIndex]}
+                      width={500}
+                      height={200}
+                      className='h-100 '
+                      style={{ width: 'auto', height: 'auto' }}
+                      alt='product images'
+                    />
+                    <i role='button' className="  fas fa-chevron-right" onClick={handleNextImage}></i>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  </>
+  );
+}
